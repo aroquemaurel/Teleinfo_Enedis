1b1e56cd9aadcb2aaba6df40c01edfea85c03822
Antoine de Roquemaurel <aroquemaurel@roquemaurel.pro>
2020-08-02 14:30:27 +0200

    Restart service if it stopped

--- restart_service.sh
+++ restart_service.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if test `find /tmp/teleinfo.error -mmin +5`
+if ! [ -f /tmp/teleinfo.pid ] 
 then
-	echo "Restart teleinfo service"
 	service teleinfo restart
+fi
+
+if [ -f /tmp/teleinfo.error ]
+then
+	if test `find /tmp/teleinfo.error -mmin +5`
+	then
+		echo "Restart teleinfo service"
+		service teleinfo restart
+	fi
 fi
