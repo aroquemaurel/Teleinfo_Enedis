f7d31b3b5bcc70a3475b47d16cf4306f95ca1b5a
Antoine de Roquemaurel <aroquemaurel@roquemaurel.pro>
2021-01-02 19:00:49 +0100

    Add a mechanism of pakets to synchronize databases

--- teleinfo/config/config.py
+++ teleinfo/config/config.py
@@ -3,3 +3,4 @@ config_serial_dev = "/dev/ttyAMA0"
 config_log_file = '/var/log/pi/teleinfo.log'
 config_errorfile = "/tmp/teleinfo.error"
 config_pidfile = "/tmp/teleinfo.pid"
+config_nb_values_trame = 500
--- teleinfo/syncdatabases.py
+++ teleinfo/syncdatabases.py
@@ -1,6 +1,7 @@
 from common.database.mysqldatabasemanager import MySQLDatabaseManager
 from common.database.sqlitedatabasemanager import SQLiteDatabaseManager 
 from models.settings import Settings
+from config.config import *
 
 global db
 
@@ -17,18 +18,30 @@ if __name__ == '__main__':
 
         Settings.singleton().database = mysql_db
         from models import models 
+        from models.settings import Logging
         max_id = mysql_db.db.session.query(mysql_db.db.func.max(models.Consumption.id)).scalar()
+        Logging.info("max_id : "+str(max_id))
         if max_id is None:
             max_id = 0
 
         Settings.singleton().database = sqlite_db 
         from models import models 
-        data_to_insert = sqlite_db.db.session.query(models.Consumption).filter(models.Consumption.id > max_id).all()
-        for data in data_to_insert:
-            consumption = models.Consumption(data)
-            mysql_db.db.session.add(consumption)
+        data_to_insert_query = sqlite_db.db.session.query(models.Consumption).filter(models.Consumption.id > max_id)
+        nb_entry = data_to_insert_query.count()
+        Logging.info("Synchronize "+str(nb_entry)+" values")
 
-        mysql_db.db.session.commit()
+        for i in range(nb_entry % config_nb_values_trame):
+            range_from = i * config_nb_values_trame 
+            range_to = (i + 1) * config_nb_values_trame 
+            Logging.info("Synchronize data from "+str(range_from) + " to "+str(range_to))
+            data_to_insert = data_to_insert_query.slice(range_from, range_to).all()
+
+            for data in data_to_insert:
+                consumption = models.Consumption(data)
+                last_insert = consumption.id
+                mysql_db.db.session.add(consumption)
+
+            mysql_db.db.session.commit()
 
     finally:
         sqlite_db.close()
